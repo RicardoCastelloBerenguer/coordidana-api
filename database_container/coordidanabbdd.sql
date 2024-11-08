2d7d69283248dd2046004c6b9f0ad2ef3da5697f
Rueskas <Rueskas@hotmail.com>
2024-11-08 19:40:25 +0100

    actualizar bbdd

--- database_container/coordidanabbdd.sql
+++ database_container/coordidanabbdd.sql
@@ -17,42 +17,13 @@
 CREATE DATABASE IF NOT EXISTS `coordidana` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
 USE `coordidana`;
 
--- Volcando estructura para evento coordidana.actualizar_prioridades
-DELIMITER //
-CREATE EVENT `actualizar_prioridades` ON SCHEDULE EVERY 1 DAY STARTS '2024-11-07 14:34:52' ON COMPLETION NOT PRESERVE ENABLE DO BEGIN
-DECLARE nueva_prioridad DECIMAL(10, 2);
-  DECLARE suma_prioridades DECIMAL(10, 2);
-  
-  -- Recalcular las prioridades para todos los tramos
-  FOR tramo_record IN (SELECT DISTINCT id_tramo FROM reportes) DO
-    -- Calcular la suma ponderada de las prioridades de los reportes, con el peso exponencial de la antigüedad
-     SELECT SUM(prioridad_ponderada) / SUM(peso_fecha) INTO nueva_prioridad
-	FROM (
-	SELECT
-        id_tramo,
-        prioridad,
-        DATEDIFF(CURRENT_DATE, fecha) AS dias_desde_reporte,
-        (1.0 / (1.0 + DATEDIFF(CURRENT_DATE, fecha))) AS peso_fecha,
-        prioridad * (1.0 / (1.0 + DATEDIFF(CURRENT_DATE, fecha))) AS prioridad_ponderada
-    FROM
-        reportes WHERE id_tramo = tramo_record.id_tramo) AS ponderado;
-   
-    UPDATE tramo
-    SET prioridad = nueva_prioridad
-    WHERE id_tramo = tramo_record.id_tramo;
-
-  END FOR;
-END//
-DELIMITER ;
-
-
 -- Volcando estructura para tabla coordidana.tramo
 CREATE TABLE IF NOT EXISTS `tramo` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `ID_TRAMO` varchar(50) NOT NULL DEFAULT '',
   `PRIORIDAD` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20084 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- La exportación de datos fue deseleccionada.
 
@@ -60,10 +31,25 @@ CREATE TABLE IF NOT EXISTS `tramo` (
 CREATE TABLE IF NOT EXISTS `usuario` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `USUARIO` varchar(50) NOT NULL DEFAULT '',
-  `PASS` varchar(50) NOT NULL DEFAULT '',
+  `PASS` varchar(64) NOT NULL DEFAULT '',
   `EMAIL` varchar(100) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `USUARIO` (`USUARIO`)
+) ENGINE=InnoDB AUTO_INCREMENT=124 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla coordidana.garaje
+CREATE TABLE IF NOT EXISTS `garaje` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID_USUARIO` int(11) NOT NULL DEFAULT 0,
+  `FECHA` datetime NOT NULL DEFAULT current_timestamp(),
+  `CODIGO` text NOT NULL,
+  `ESTADO` int(11) NOT NULL,
+  `COMENTARIO` varchar(400) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_GARAJE_ID_USUARIO` (`ID_USUARIO`),
+  CONSTRAINT `FK_GARAJE_ID_USUARIO` FOREIGN KEY (`ID_USUARIO`) REFERENCES `usuario` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- La exportación de datos fue deseleccionada.
 
@@ -83,9 +69,39 @@ CREATE TABLE IF NOT EXISTS `reportes` (
   PRIMARY KEY (`ID`),
   KEY `FK_ID_USUARIO` (`ID_USUARIO`),
   CONSTRAINT `FK_ID_USUARIO` FOREIGN KEY (`ID_USUARIO`) REFERENCES `usuario` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=159 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para evento coordidana.procesar_tramos_diario
+DELIMITER //
+CREATE EVENT `procesar_tramos_diario` ON SCHEDULE EVERY 1 SECOND STARTS '2024-11-08 16:04:47' ON COMPLETION NOT PRESERVE ENABLE DO BEGIN
+    
+	UPDATE tramo t
+	INNER JOIN (
+	    SELECT
+	        id_tramo,
+	        CASE
+	            WHEN MAX(CASE WHEN prioridad = 1 THEN 1 ELSE 0 END) = 1 THEN 1
+	            ELSE SUM(prioridad_ponderada) / SUM(peso_fecha)
+	        END AS nueva_prioridad
+	    FROM (
+	        SELECT
+	            id_tramo,
+	            prioridad,
+	            DATEDIFF(CURRENT_DATE, fecha) AS dias_desde_reporte,
+	            (1.0 / (1.0 + DATEDIFF(CURRENT_DATE, fecha))) AS peso_fecha,
+	            prioridad * (1.0 / (1.0 + DATEDIFF(CURRENT_DATE, fecha))) AS prioridad_ponderada
+	        FROM
+	            reportes
+	    ) AS calculos
+	    GROUP BY
+	        id_tramo
+	) AS prioridad_calculada ON t.id_tramo = prioridad_calculada.id_tramo
+	SET t.prioridad = prioridad_calculada.nueva_prioridad;
+    
+END//
+DELIMITER ;
 
 -- Volcando estructura para disparador coordidana.actualizar_prioridad
 SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
@@ -117,11 +133,11 @@ CREATE TRIGGER `actualizar_prioridad` AFTER INSERT ON `reportes` FOR EACH ROW BE
   END IF;
 
   -- Verificar si el tramo ya existe
-  IF EXISTS (SELECT 1 FROM tramo WHERE id = NEW.id_tramo) THEN
+  IF EXISTS (SELECT 1 FROM tramo WHERE id_tramo = NEW.id_tramo) THEN
     -- Si existe, actualizar la prioridad
     UPDATE tramo
     SET prioridad = nueva_prioridad
-    WHERE id = NEW.id_tramo;
+    WHERE id_tramo = NEW.id_tramo;
   ELSE
     -- Si no existe, insertar el nuevo tramo con la prioridad calculada
     INSERT INTO tramo (id_TRAMO, prioridad)
